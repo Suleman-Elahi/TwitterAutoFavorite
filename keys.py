38a72674fad2c6e681431766b99c4b8bc55e30a7
Suleman <slmn95@rediffmail.com>
2017-12-12 20:48:14 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,8 +2,8 @@
 #keys.py
 #visit http://dev.twitter.com to create an application and get your keys
 keys = dict(
-	consumer_key='4BBFloPC3xBKjGJ4TgiWP2FJn',
-	consumer_secret='Ips22fjUSEf5MgPiNnx2C7Qwnp3B5tMOIaBWRLhsTvn5PoYZ2s',
-	access_token='729705595993882624-gf8AXpsxiyBfKIn8PBH66o21RW6nFWE',
-	access_token_secret='eUY6nf1U0yaqtl8A9HAXMpboo90ndcmyO5gZjzm66pb7B',
+	consumer_key='',
+	consumer_secret='',
+	access_token='',
+	access_token_secret='',
 	)
