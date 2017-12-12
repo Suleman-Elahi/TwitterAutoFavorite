67a298fa44ef90cb2bcad8ae7ad7eb47d6763e69
Suleman <slmn95@rediffmail.com>
2017-12-12 20:47:28 +0530

    Add source code

---
+++ bot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import tweepy
+#from our keys module (keys.py), import the keys dictionary
+from keys import keys
+ 
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+ 
+twt = api.search("#Salvatore",result_type="mixed",count=5) 
+ 
+for s in twt:
+    api.create_favorite(s.id)
+    print(s.id)
+print("Done!!!")
